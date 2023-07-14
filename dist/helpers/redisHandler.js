b5c80162cf8922633d48484033149fff3a19992c
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2023-07-14 22:53:08 +0530

    ::bug:: => redis connection timeout

--- dist/helpers/redisHandler.js
+++ dist/helpers/redisHandler.js
@@ -1,3 +1,5 @@
 import { createClient } from 'redis';
 import Bio from '../data/bio.json' assert { type: "json" };
-export const redi = createClient(Bio.REDIS.CONNECT);
+export const redi = createClient(Object.assign(Object.assign({}, Bio.REDIS.CONNECT), { socket: {
+        connectTimeout: 100 * 1000
+    } }));
--- helpers/redisHandler.ts
+++ helpers/redisHandler.ts
@@ -1,4 +1,9 @@
 import { createClient } from 'redis';
 import Bio from '../data/bio.json' assert { type: "json" };
 
-export const redi = createClient(Bio.REDIS.CONNECT);
+export const redi = createClient({
+    ...Bio.REDIS.CONNECT,
+    socket: {
+        connectTimeout: 100 * 1000
+    }
+});
